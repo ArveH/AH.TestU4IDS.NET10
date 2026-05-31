390f3cf8803bfd71aa335b8690ffac78b87a4809
Arve Hansen <arve.hansen@unit4.com>
2026-05-31 19:41:13 +0200

    Added new template web api

--- AH.TestU4IDS.NET10.AppHost/AppHost.cs
+++ AH.TestU4IDS.NET10.AppHost/AppHost.cs
@@ -9,4 +9,6 @@ builder.AddProject<Projects.AH_TestU4IDS_NET10_Web>("webfrontend")
     .WithReference(parentApi)
     .WaitFor(parentApi);
 
+builder.AddProject<Projects.AH_TestU4IDS_NET10_WeatherApi>("ah-testu4ids-net10-weatherapi");
+
 builder.Build().Run();
